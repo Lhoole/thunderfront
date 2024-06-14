0952f2ba4121f430cd3137b910a59dfc33c012a8
Luke <116241757+Nyrexis@users.noreply.github.com>
2024-06-14 13:04:38 +1000

    micSVG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "./header";
+import MicSVG from "./micSVG";
 import SendIcon from "./sendIcon";
 
 export default function HomePage() {
@@ -15,6 +16,12 @@ export default function HomePage() {
                 placeholder="Learn about ThunderLabs"
                 className="w-full rounded-2xl border-2 border-[#FF0000] px-4 py-2 text-black"
               />
+              <button
+                className="absolute right-10 top-1/2 h-[30px] w-[30px] -translate-y-1/2 transform"
+                type="submit"
+              >
+                <MicSVG />
+              </button>
               <button
                 className="absolute right-2 top-1/2 h-[30px] w-[30px] -translate-y-1/2 transform"
                 type="submit"
